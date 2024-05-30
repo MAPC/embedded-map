cc81d7469cbbb8b5e2cae9e658cc46c832c39b2e
NotLiu <aliu@lediur.com>
2024-05-30 16:25:55 -0500

    get map implemented, and all feature paths styled matching legend in landlines 2.0 (TO BE FIXED), implement poly simplification on zoom for performance

--- src/Map.js
+++ src/Map.js
@@ -8,7 +8,7 @@ import Spinner from "react-bootstrap/Spinner";
 
 import arcgisPbfDecode from "arcgis-pbf-parser";
 
-import { MapContainer, TileLayer, ZoomControl, GeoJSON, Circle } from "react-leaflet";
+import { MapContainer, TileLayer, ZoomControl, GeoJSON, Circle, useMapEvents } from "react-leaflet";
 
 import "leaflet/dist/leaflet.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -16,6 +16,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // Importing "for side effects", i.e., to extend leaflet with smooth scrolling
 import "./SmoothScroll";
 import { polygon } from "leaflet";
+import { FeatureLayer } from "react-esri-leaflet";
 
 // constants
 const MAP_DB = "MapCache";
@@ -256,86 +257,6 @@ export const queryFeatureService = async ({ serviceName, token = null, layerID =
  *   }).then((points) => setPoints(points));
  *
  */
-export const queryAirtableBase = async ({
-  serviceName,
-  token = null,
-  baseID = null,
-  tableName = null,
-  viewName = null,
-  sortOptions = [],
-  count = null,
-  fields = [],
-  force = false,
-  latitudeField = null,
-  longitudeField = null,
-}) => {
-  const cacheKey = getCacheKey(baseID, tableName);
-  let featureCollection = await readFeatureCollection(cacheKey);
-  if (!force && featureCollection != null) {
-    // Return cached version if we have it
-    return featureCollection;
-  }
-
-  if (token == null) {
-    console.error("No AirTable token provided: unable to fetch data from API");
-    return [];
-  }
-
-  let cmsBase;
-  if (token == null) {
-    // Read token from env if not passed
-  } else {
-    cmsBase = new Airtable({ apiKey: token }).base(baseID);
-  }
-
-  const featuresList = [];
-  const selectOptions = {};
-  if (viewName != null) {
-    selectOptions.view = viewName;
-  }
-  if (sortOptions != null) {
-    selectOptions.sort = sortOptions;
-  }
-  const table = await cmsBase(tableName).select(selectOptions);
-
-  const response = await table.eachPage((records, fetchNextPage) => {
-    records.forEach(function (record) {
-      let coordinates = [];
-      if (latitudeField == null || longitudeField == null) {
-        // Can't set coordinates if we don't know the lat/long
-        console.warn("latitudeField/longitudeField not defined: returning GeoJSON without coordinates!");
-      } else {
-        const longitude = record.fields[longitudeField];
-        const latitude = record.fields[latitudeField];
-        coordinates = [longitude, latitude];
-      }
-      // delete record.Long;
-      // delete record.Lat;
-      const feature = {
-        type: "Feature",
-        geometry: {
-          type: "Point",
-          coordinates,
-        },
-        properties: { ...record.fields },
-      };
-      featuresList.push(feature);
-    });
-    fetchNextPage();
-  });
-
-  // Only fetch new data from server if read from IndexedDB is not successful
-  featureCollection = {
-    type: "FeatureCollection",
-    name: cacheKey,
-    crs: { type: "name", properties: { name: "EPSG:4326" } },
-    features: featuresList,
-  };
-
-  writeFeatureCollection(featureCollection);
-
-  return featureCollection;
-};
 
 const LoadingOverlay = styled.div`
   position: absolute;
@@ -365,17 +286,35 @@ const Wrapper = styled.div`
     height: ${(props) => props.height};
   }
 `;
+const MapEventsHandler = ({ setZoom }) => {
+  const map = useMapEvents({
+    zoomend: () => {
+      setZoom(map.getZoom());
+    },
+  });
+  return null;
+};
 
+function setSimplifyFactor(zoom) {
+  if (zoom >= 10) {
+    return 0.5;
+  } else if (zoom >= 5) {
+    return 0.25;
+  }
+
+  return 0;
+}
 export const MAPCMap = ({ wrapperHeight = "100vh", mapFocus = "region", points = [], mapboxToken = process.env.REACT_APP_MAPBOX_TOKEN }) => {
   const [polygons, setPolygons] = useState([]);
+  const [zoom, setZoom] = useState(10);
+
   useEffect(() => {
     const loadPolygons = async () => {
       // constants and feature query setup
-      const MUNI_POLYGONS = "simplified_muni_polygons_2";
       const clientId = process.env.REACT_APP_AGOL_CLIENT_ID;
       const clientSecret = process.env.REACT_APP_AGOL_CLIENT_SECRET;
       const token = await authenticateEsri(clientId, clientSecret);
-      const serviceName = MUNI_POLYGONS;
+      const serviceName = "simplified_muni_polygons_2";
       const polygonData = await queryFeatureService({ token, serviceName, force: true });
 
       setPolygons([
@@ -388,6 +327,7 @@ export const MAPCMap = ({ wrapperHeight = "100vh", mapFocus = "region", points =
               weight: 0.8,
               fillOpacity: 0.5,
               opacity: 0.5,
+              zIndex: -1,
             };
           },
           data: polygonData.features,
@@ -396,6 +336,7 @@ export const MAPCMap = ({ wrapperHeight = "100vh", mapFocus = "region", points =
     };
     loadPolygons();
   }, []);
+
   let focusProps = regionMapProps; // Default: MAPC regional map
   if (mapFocus === "state") {
     focusProps = stateMapProps;
@@ -437,7 +378,11 @@ export const MAPCMap = ({ wrapperHeight = "100vh", mapFocus = "region", points =
       }
     }
   }
-  console.log(process.env.REACT_APP_MAPBOX_TOKEN);
+
+  const pathWeight = 4.0;
+
+  // Hook to handle map events
+
   return (
     <Wrapper height={wrapperHeight}>
       <MapContainer
@@ -449,8 +394,99 @@ export const MAPCMap = ({ wrapperHeight = "100vh", mapFocus = "region", points =
         smoothSensitivity={2.5} // zoom speed. default is 1
       >
         <TileLayer url={createTileURL("light-v10", mapboxToken)} attribution={mapboxAttribution} tileSize={512} zoomOffset={-1} />
-        {layers}
+        {/* {layers} */}
+
         <ZoomControl position="bottomright" />
+        <MapEventsHandler setZoom={setZoom} />
+        <FeatureLayer
+          url="https://geo.mapc.org/server/rest/services/transportation/landlines/FeatureServer/0"
+          simplifyFactor={setSimplifyFactor(zoom)}
+          style={(feature) => {
+            let colorRow;
+            let dashArray;
+            if (feature.properties.seg_type == 1) {
+              colorRow = "#00a884";
+            } else if (feature.properties.seg_type == 2) {
+              colorRow = "#00a884";
+            } else if (feature.properties.seg_type == 3) {
+              colorRow = "#00a884";
+              dashArray = "3,8";
+            } else if (feature.properties.seg_type == 4) {
+              colorRow = "#c7d79e";
+            } else if (feature.properties.seg_type == 5 || feature.properties.seg_type == 6) {
+              colorRow = "#0170ff";
+            } else if (feature.properties.seg_type == 7 || feature.properties.seg_type == 8) {
+              colorRow = "#73b2ff";
+            } else if (feature.properties.seg_type == 9 || feature.properties.seg_type == 10) {
+              colorRow = "#d7c29e";
+            } else if (feature.properties.seg_type == 11) {
+              colorRow = "#d7c29e";
+              dashArray = "3,8";
+            } else if (feature.properties.seg_type == 12) {
+              colorRow = "#ffed7f";
+              dashArray = "3,8";
+            } else if (feature.properties.seg_type == 13) {
+              colorRow = "#ff5b0a";
+            } else if (feature.properties.seg_type == 14) {
+              colorRow = "#ff5b0a";
+              // dashArray = "3,8";
+            } else if (feature.properties.seg_type == 15) {
+              colorRow = "#ff732d";
+            } else if (feature.properties.seg_type == 16) {
+              colorRow = "#ff732d";
+              dashArray = "3,8";
+            }
+
+            return {
+              color: colorRow,
+              stroke: colorRow,
+              weight: pathWeight,
+              fillOpacity: 0,
+              opacity: 1,
+              dashArray: dashArray,
+              dashOffset: "0",
+            };
+          }}
+          pane="tilePane"
+        />
+        {/* inverted dash extra layer */}
+        <FeatureLayer
+          url="https://geo.mapc.org/server/rest/services/transportation/landlines/FeatureServer/0"
+          simplifyFactor={setSimplifyFactor(zoom)}
+          style={(feature) => {
+            let colorRow;
+            let dashArray;
+            if (feature.properties.seg_type == 2) {
+              colorRow = "white";
+              dashArray = "3,8";
+            } else if (feature.properties.seg_type == 5) {
+              colorRow = "white";
+            } else if (feature.properties.seg_type == 6) {
+              colorRow = "white";
+              dashArray = "3,8";
+            } else if (feature.properties.seg_type == 8) {
+              colorRow = "white";
+              dashArray = "3,8";
+            } else if (feature.properties.seg_type == 9) {
+              colorRow = "white";
+              dashArray = "3,8";
+            } else if (feature.properties.seg_type == 15 || feature.properties.seg_type == 16) {
+              colorRow = "white";
+              dashArray = "3,8";
+            }
+
+            return {
+              color: colorRow,
+              stroke: colorRow,
+              weight: pathWeight - 2.0,
+              fillOpacity: 0,
+              opacity: 1,
+              dashArray: dashArray,
+              dashOffset: "0",
+            };
+          }}
+          pane="tilePane"
+        />
       </MapContainer>
     </Wrapper>
   );
